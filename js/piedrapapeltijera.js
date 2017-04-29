402787fe1e0afefa2c25635f5941e98f6b8f4bbb
Exequiel Hernando <exequielhernando@gmail.com>
2017-04-29 12:46:22 -0300

    Juego terminado

--- js/piedrapapeltijera.js
+++ js/piedrapapeltijera.js
@@ -3,22 +3,26 @@ let jugadaUsuario;
 let checkbox = document.getElementById("check");
 console.log(checkbox.checked);
 let eleccionAnteriorPc="Piedra";
+let contadorpartidas=1;
 
-
-function elegirJugadaPc() {
+function jugadaAlternativa() {
 		if (eleccionAnteriorPc=="Piedra"){
 		eleccionAnteriorPc="Papel"
 		return "Papel";
-}
+	}
+
 	else if (eleccionAnteriorPc=="Papel"){
-
+		eleccionAnteriorPc="Tijera"
+		return "Tijera";
 	}
-
-	}
-}
+	else if (eleccionAnteriorPc=="Tijera"){
+		eleccionAnteriorPc="Piedra"
+		return "Piedra";
+	}}
 function obtenerJugadaUsuario(seleccion) {
 	console.log(seleccion);
 	jugadaUsuario=seleccion;
+	jugar()
 }
 function obtenerJugadaPc(){
 	let azar = Math.floor((Math.random()*3)+1);
@@ -33,16 +37,20 @@ function obtenerJugadaPc(){
     return respuesta;
 }
 function jugar(){
-	let jugadaPc=obtenerJugadaPc();
+	let jugadaPc;
 	let mensaje;
+	let mensajeTabla;
 	let contenedorMensaje=document.getElementById("resultado");
-	while (checkbox.checked) {
-		jugadaPc=elegirJugadaPc();
-
+	if (checkbox.checked) {
+		jugadaPc=jugadaAlternativa();
+	}
+	else {
+		 jugadaPc=obtenerJugadaPc();
 	}
 	if(jugadaUsuario == jugadaPc)
 	{
 		mensaje = "<h2>¡Empataste!</h2>";
+		mensajeTabla="Empate";
 	}
 
 	else if(jugadaUsuario == "Piedra" && jugadaPc == "Tijera"
@@ -54,11 +62,25 @@ function jugar(){
 		)
 	{
 		mensaje = "<h2>¡Ganaste!</h2>";
+		mensajeTabla="Usuario";
 	}
 	//En el resto de posibilidades se pierde.
 	else
 	{
 		mensaje = "<h2>¡Perdiste!</h2>";
+		mensajeTabla="Computadora";
 	}
 	contenedorMensaje.innerHTML = mensaje;
+		actualizarTabla(mensajeTabla);
+	}
+	function actualizarTabla(mensaje) {
+		let ganador;
+		let resultadoJugada=document.getElementById("tablaResultado");
+		let fila=resultadoJugada.insertRow(contadorpartidas);
+		let columna1=fila.insertCell(0);
+		let columna2=fila.insertCell(1);
+		columna1.innerHTML=contadorpartidas;
+		columna2.innerHTML=mensaje;
+
+		contadorpartidas++;
 	}
